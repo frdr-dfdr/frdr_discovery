48dbb78fc26ec418b097c375fae37d072f2d62fd
Todd Trann <todd.trann@usask.ca>
2016-09-09 10:17:29 -0600

    Fix syntax

--- www/js/src/ng/app/shared/services/esSearch.js
+++ www/js/src/ng/app/shared/services/esSearch.js
@@ -83,7 +83,7 @@ define(function(require){
                         if(website_env !== 'prod') {
                             console.log('response:', response);
                         }
-                        if !(response.hasOwnProperty('_gfacets')) { response._gfacets = ""; }
+                        if (!response.hasOwnProperty('_gfacets')) { response.['_gfacets'] = ""; }
                         var output = {
                             results: response.['_gmeta'],
                             count  : response.['_gstats.count'],
