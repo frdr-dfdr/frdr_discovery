335c7d2672a837ab9d9605d1a461cfbe1cc41fd1
Todd Trann <todd.trann@usask.ca>
2016-09-16 10:38:38 -0600

    Debugging

--- www/js/src/ng/app/shared/thumbnails/thumbnails.js
+++ www/js/src/ng/app/shared/thumbnails/thumbnails.js
@@ -25,7 +25,24 @@ define(function (require) {
 
             //$scope.src = thumbService.thumbSrc($scope.width, $scope.repo, $scope.itemId, $scope.colNick, $scope.handle);
             $scope.src = $scope.imgSrc;
-            
+
+            // TEMPORARY FOR SC DEMO
+            if ($scope.src == "http://static.lib.sfu.ca/clf2013/sfu-logo.png") {
+                $scope.src = "https://rdmtest1.computecanada.ca/sfu_80x80.png";
+            }
+            if ($scope.src == "https://circle-23jan2015.sites.olt.ubc.ca/files/2015/01/circle-logo-inverted.png") {
+                $scope.src = "https://rdmtest1.computecanada.ca/ubc_80x80.png";
+            }
+            if ($scope.src == "https://rdmtest1.computecanada.ca/jspui/image/logo.png") {
+                $scope.src = "https://rdmtest1.computecanada.ca/frdr_80x80.png";
+            }
+            if ($scope.src == "http://images.scholarsportal.info/dataverse/logo.png") {
+                $scope.src = "https://rdmtest1.computecanada.ca/sp_80x80.png";
+            }
+            if ($scope.src == "http://open.canada.ca/data/static/img/leaves/fivestar.png") {
+                $scope.src = "https://rdmtest1.computecanada.ca/odc_80x80.png";
+            }
+
             if(!$scope.src) { $scope.noThumb = true;}
             if($scope.itemId) {
                 collectionData.getTitle($scope.colNick).then(function(response){
