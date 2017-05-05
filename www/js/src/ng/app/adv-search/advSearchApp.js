4d8d7092b05c1d177b7f99780e93ae24f87082d2
Todd Trann <todd.trann@usask.ca>
2017-05-05 15:59:21 -0600

    Link from adv search to docs

--- www/js/src/ng/app/adv-search/advSearchApp.js
+++ www/js/src/ng/app/adv-search/advSearchApp.js
@@ -40,6 +40,7 @@ define(function(require){
 
         $translateProvider.translations('en', {
             'ADVSEARCH_HEADER':'Advanced Search',
+            'ADVSEARCH_DOCS': 'Help for this page is available <a href="/docs/en/using_frdr/#searching">in the documentation</a>',
             'ALL_FIELDS': 'All fields',
             'ALL_SOURCES': 'All sources',
             'ANYALL_ALL': 'all of these words:', 
@@ -91,6 +92,7 @@ define(function(require){
              
         $translateProvider.translations('fr', {
             'ADVSEARCH_HEADER':'Recherche Avancée',
+            'ADVSEARCH_DOCS': 'Pour obtenir de l%27aide sur cette page, <a href="/docs/fr/frdr_d%27utilisation/">lisez la documentation</a>',
             'ALL_FIELDS': 'Toutes les champs',
             'ALL_SOURCES': 'Toutes les sources',
             'ANYALL_ALL': 'tous ces mots:',
